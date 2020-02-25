b82e8ad05a745338a39353e0ad4da18efa0d21ab
lawsont <lawson.thalmann@kellogg.northwestern.edu>
2020-02-25 12:07:33 +0000

    Tuesday morning

--- app.rb
+++ app.rb
@@ -11,8 +11,12 @@ ForecastIO.api_key = "d84bbf5ff006b5a337049bd163331583"
 
 get "/" do
   # show a view that asks for the location
+  	view "ask"
 end
 
 get "/news" do
   # do everything else
+    results = Geocoder.search(params["q"])
+    lat_long = results.first.coordinates # => [lat, long]
+    "#{lat_long[0]} #{lat_long[1]}"
 end
